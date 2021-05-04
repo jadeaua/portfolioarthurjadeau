4c7d4f6f8e7c56ef674e40f010bccb476ba7d61c
jadeaua <72965310+jadeaua@users.noreply.github.com>
2021-05-04 14:49:05 +0200

    accueil

---
+++ accueil.php
@@ -0,0 +1,55 @@
+
+
+            <div class="section">
+              <div class="container">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="section" id="about">
+        <div class="container">
+          <div class="card" data-aos="fade-up" data-aos-offset="10">
+            <div class="row">
+              <div class="col-lg-6 col-md-12">
+                <div class="card-body">
+                  <div class="h4 mt-0 title">
+                    Profil
+                  </div>
+                  <p>Bonjour ! Je m'appelle Arthur Jadeau. Je suis passionnée d'informatique, j'aime apprendre et
+                    acquérir de nouvelles connaissances.</p>
+                  <p>Je passe beaucoup de temps sur internet pour me former le plus possible.</p>
+                </div>
+              </div>
+              <div class="col-lg-6 col-md-12">
+                <div class="card-body">
+                  <div class="h4 mt-0 title">Information</div>
+                  <div class="row">
+                    <div class="col-sm-4"><strong class="text-uppercase">Age:</strong></div>
+                    <div class="col-sm-8">19 ans</div>
+                  </div>
+                  <div class="row mt-3">
+                    <div class="col-sm-4"><strong class="text-uppercase">Email:</strong></div>
+                    <div class="col-sm-8"><a href="mailto:leo.bessone@ecole-isitech.fr">arthurjadeau@gmail.com</a>
+                    </div>
+                  </div>
+                  <div class="row mt-3">
+                    <div class="col-sm-4"><strong class="text-uppercase">Téléphone:</strong></div>
+                    <div class="col-sm-8"><a href="tel:+33769833751">0768050758</a></div>
+                  </div>
+                  <div class="row mt-3">
+                    <div class="col-sm-4"><strong class="text-uppercase">Adresse:</strong></div>
+                    <div class="col-sm-8">12 rue de Chateaubriand - 49300 Cholet</div>
+                  </div>
+                  <div class="row mt-3">
+                    <div class="col-sm-4"><strong class="text-uppercase">Langue:</strong></div>
+                    <div class="col-sm-8">Français, Anglais</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      
